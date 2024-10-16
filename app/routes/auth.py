4bfd81afc0b25e0c17e9af31eb823b1b3478cb23
Marie <mje.espinosa@gmail.com>
2024-10-16 21:46:34 +0200

    refacto

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -6,46 +6,33 @@ from app.utils.jwt import create_jwt
 
 auth_route = Blueprint('authentification route', __name__)
 
-@auth_route.route('/register', methods=(['POST']))
+@auth_route.route('/register', methods=['POST'])
 def register():
+    try:
         json_data, error = get_json_data()
 
         if error:
             return generate_response(error, error="Bad request", status=400)
-        
-        result = validate_json_schema(json_data=json_data, schema=auth_schema)
 
-        try :
-            existing_user = UserService.get_user_by_mail(result["email"])
+        validate_json_schema(json_data=json_data, schema=auth_schema)
 
-            if existing_user:
-                return generate_response(message="Mail already taken", status=400, error="Conflict")
+        res = UserService.register(json_data)
 
-            user = UserService.create_user(
-                email=result["email"],
-                firstname=result["firstname"],
-                lastname=result["lastname"],
-                password=result["password"]
-            )
+        return res
 
-            return generate_response(message="User created", status=201)
-        except Exception as e:
-             return generate_response(message="An error occurred", status=500, error="Internal Server Error")
-        
+    except Exception as e:
+        return generate_response(message="An error occurred", status=500, error="Internal Server Error")
+
 
 @auth_route.route('/login', methods=(['POST']))
 def login():
     json_data, error = get_json_data()
-
     if error:
         return generate_response(message="Bad request", error=error, status=400)
-    
-    result = validate_json_schema(json_data=json_data, schema=login_schema)
+    data = validate_json_schema(json_data=json_data, schema=login_schema)
     try:
-        user  = UserService.verify_user_login(email=result["email"], password=result["password"])
-        if user:
-            token = create_jwt(data=result["email"])
-            return generate_response(message="Login successful", status=200, data={"token": token})
+        res = UserService.login(email=data["email"], password=data["password"])
+        return res
     except Exception as e:
         return generate_response(message="An error occurred during login", status=500, error=str(e))
 
--- app/services/user.py
+++ app/services/user.py
@@ -2,50 +2,75 @@ from app.db import db
 from app.models.user import User
 from app.utils.hash import verify_password
 from app.utils.jwt import create_jwt
-from flask_jwt_extended import create_access_token
 from app.utils.request import generate_response
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+from app.utils.jwt import create_jwt
+from flask import jsonify
+from app.schemas.user import auth_schema
 
 
 class UserService:
-    def create_user(email, firstname, lastname, password):
-        try:
-            user = User(
-                email=email,
-                firstname=firstname,
-                lastname=lastname,
-                password=password
-            )
-            db.session.add(user)
-            db.session.commit()
+      
+    def login(email : str, password : str):
+        """ Log user
 
-            return user
+        Parameters
+        -----------------
+        email : email provided
+        password : password provided
 
-        except Exception as error:
-            db.session.rollback()
-            return error
-        
-    def get_user_by_mail(email):
-        try:
-            return User.query.filter_by(email=email).first()
-        except Exception as error:
-            db.session.rollback()
-            return error
-        
-    def verify_user_login(email, password):
+        Returns
+        ----------
+        token 
+        """
         try:
             user = User.get_user_by_mail(email=email)
             if not user :
                 generate_response(message="Email not found", error="Bad Request", status=400)
             else : 
                 logged = user.authenticate(password_to_check=password)
-                return logged
+                if logged:
+                    token = create_jwt(data=email)
+                    return generate_response(message="Login successful", data=token, status=200)
+                else:
+                    return generate_response(message="Wrong password", error="Unauthorized", status=401)
         except Exception as error:
             print(error)
             raise error
+        
+        
+    def register(user : auth_schema):
+        """ Create user and hash password
+
+        Parameters
+        -----------------
+        email : email provided
+        password : password provided
+        firstname : firstname
+        lastname : lastname
+
+        Returns
+        ----------
+        token 
+        """
+                
+        existing_user = User.get_user_by_mail(email=user["email"])
+        if existing_user:
+            return generate_response(message="Mail already taken", status=400, error="Conflict")
+        try:
+            user = User(
+                email=user["email"],
+                firstname=user["firstname"],
+                lastname=user["lastname"],
+                password=user["password"]
+            )
+            db.session.add(user)
+            db.session.commit()
+
+            return generate_response(message="User created", status=201)
+
+        except Exception as error:
+            db.session.rollback()
+            return error
 
 
       
