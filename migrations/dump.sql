e32404122f8ea9093d335b08a1da9c0ce97cda06
Marie <mje.espinosa@gmail.com>
2024-10-29 16:37:35 +0100

    remove capital

--- migrations/dump.sql
+++ migrations/dump.sql
@@ -1,6 +1,5 @@
 CREATE TABLE "categorie"(
     "id" SERIAL  NOT NULL,
-    "budget_id" BIGINT NULL,
     "type" VARCHAR(255) CHECK
         ("type" IN('expense', 'income')) NOT NULL DEFAULT 'expense',
         "name" VARCHAR(255) NOT NULL,
@@ -12,7 +11,7 @@ ALTER TABLE
 CREATE TABLE "transaction"(
     "id" SERIAL  NOT NULL,
     "budget_id" BIGINT NOT NULL,
-    "categorie_id" BIGINT NOT NULL,
+    "categorie_id" BIGINT,
     "amount" DECIMAL(10, 2) NOT NULL,
     "transaction_date" DATE NOT NULL,
     "type" VARCHAR(255) CHECK
@@ -22,7 +21,7 @@ CREATE TABLE "transaction"(
 );
 ALTER TABLE
     "transaction" ADD PRIMARY KEY("id");
-CREATE TABLE "Budget"(
+CREATE TABLE "budget"(
     "id" SERIAL NOT NULL,
     "user_id" BIGINT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
@@ -30,7 +29,7 @@ CREATE TABLE "Budget"(
     "updated_at" DATE
 );
 ALTER TABLE
-    "Budget" ADD PRIMARY KEY("id");
+    "budget" ADD PRIMARY KEY("id");
 CREATE TABLE "user"(
     "id" SERIAL NOT NULL,
     "firstname" VARCHAR(255) NULL,
@@ -45,13 +44,11 @@ ALTER TABLE
 ALTER TABLE
     "user" ADD CONSTRAINT "user_email_unique" UNIQUE("email");
 ALTER TABLE
-    "Budget" ADD CONSTRAINT "budget_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "user"("id");
+    "budget" ADD CONSTRAINT "budget_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "user"("id");
 ALTER TABLE
     "transaction" ADD CONSTRAINT "transaction_categorie_id_foreign" FOREIGN KEY("categorie_id") REFERENCES "categorie"("id");
 ALTER TABLE
-    "categorie" ADD CONSTRAINT "categorie_budget_id_foreign" FOREIGN KEY("budget_id") REFERENCES "Budget"("id");
-ALTER TABLE
-    "transaction" ADD CONSTRAINT "transaction_budget_id_foreign" FOREIGN KEY("budget_id") REFERENCES "Budget"("id");
+    "transaction" ADD CONSTRAINT "transaction_budget_id_foreign" FOREIGN KEY("budget_id") REFERENCES "budget"("id");
 
 
 -- Insérer des catégories dans la table "categorie"
