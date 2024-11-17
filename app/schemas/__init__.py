43c74329b26b8d21440223a4c9a178907d8ed613
marie <mje.espinosa@gmail.com>
2024-11-17 18:29:05 +0100

    import schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,7 @@
 from .user import auth_schema, login_schema
 from .budget import create_budget_payload
 from marshmallow import Schema, fields, validate
+from .transaction import transaction_schema
 
 
 # used in categorie or transaction
